3dfaebeb930cc3a321ebca022c38fbc15c6e8758
Amrita Mukherjee <code.jmamrita@gmail.com>
2023-05-09 23:23:31 +0530

    added picker component

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState([]);
+  const [items, setItems] = useState([
+    { label: "Spain", value: "spain" },
+    { label: "Madrid", value: "madrid" },
+    { label: "Barcelona", value: "barcelona" },
+    { label: "Italy", value: "italy" },
+    { label: "Rome", value: "rome" },
+    { label: "Finland", value: "finland" },
+  ]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js</Text>
+      <View
+        style={{
+          backgroundColor: "#171717",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          paddingHorizontal: 15,
+        }}
+      >
+        <DropDownPicker
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+          multiple={true}
+          mode="BADGE"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +45,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
