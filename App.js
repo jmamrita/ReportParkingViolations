ba1df108eae02e410020e9b4ee884f588e1e8aca
Amrita Mukherjee <code.jmamrita@gmail.com>
2023-05-10 13:48:39 +0530

    added camera libraries to project

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Button, Image, View, Text } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import DropDownPicker from "react-native-dropdown-picker";
 
 export default function App() {
@@ -14,12 +15,52 @@ export default function App() {
     { label: "Rome", value: "rome" },
     { label: "Finland", value: "finland" },
   ]);
+  const [image, setImage] = useState(null);
+  const [hasCameraPermission, setCameraPermission] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const cameraPermission =
+        await ImagePicker.requestCameraPermissionsAsync();
+      setCameraPermission(cameraPermission === "granted");
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const takeImage = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("You've refused to allow this appp to access your camera!");
+      return;
+    }
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result);
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js</Text>
       <View
         style={{
-          backgroundColor: "#171717",
           flex: 1,
           alignItems: "center",
           justifyContent: "center",
@@ -36,6 +77,18 @@ export default function App() {
           multiple={true}
           mode="BADGE"
         />
+      </View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+      </View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Take an image from camera" onPress={takeImage} />
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
       </View>
       <StatusBar style="auto" />
     </View>
